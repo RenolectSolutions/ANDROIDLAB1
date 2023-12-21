b00d6566909eca4ce84906ffa8c86fa444e4fc49
Richa <RenolectSolutions@gmail.com>
2023-12-21 18:55:52 +0200

    First commit.

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonShowText;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonShowText = findViewById(R.id.buttonShowText);
+        textView = findViewById(R.id.textView);
+
+        buttonShowText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code to execute when the button is clicked
+                textView.setText("MIPT 1");
+            }
+        });
     }
 }
