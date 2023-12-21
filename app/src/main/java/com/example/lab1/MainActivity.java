577a86c858cfb33ea0737f3501c1a2d4db92533f
Richa <RenolectSolutions@gmail.com>
2023-12-21 19:09:37 +0200

    Revert "First commit."
    This reverts commit b4f381ebd3ec233099a93d0c0464de1f80c5c628.

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -6,12 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonShowText;
-    private Button buttonChangeColor;
     private TextView textView;
 
     @Override
@@ -20,26 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonShowText = findViewById(R.id.buttonShowText);
-        buttonChangeColor = findViewById(R.id.buttonChangeColor);
         textView = findViewById(R.id.textView);
 
         buttonShowText.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code to execute when the button is clicked
                 textView.setText("MIPT 1");
-            }
-        });
-
-        buttonChangeColor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Change text color to a random color
-                int randomColor = Color.rgb(
-                        (int) (Math.random() * 255),
-                        (int) (Math.random() * 255),
-                        (int) (Math.random() * 255)
-                );
-                textView.setTextColor(randomColor);
             }
         });
     }
